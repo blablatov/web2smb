137c76de5e20af3d1b8a6795764393c8a0c86aa4
Андрей <96768789+blablatov@users.noreply.github.com>
2022-03-25 13:49:31 +0500

    Add files via upload

---
+++ http_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+)
+
+var _ http.RoundTripper = (*RoundTrip)(nil)
+
+type RoundTrip func(r *http.Request) (*http.Response, error)
+
+func (rt RoundTrip) RoundTrip(r *http.Request) (*http.Response, error) {
+	return rt(r)
+}
